aecd84178c3a9401f3e755197da2c323522f9268
Layka47 <chris47.27@proton.me>
2024-08-21 20:35:52 -0700

    Added TODO to replace column headers with row 1

--- 01-aqspec-data-scrape.R
+++ 01-aqspec-data-scrape.R
@@ -5,12 +5,16 @@ pacman::p_load(pacman, rvest, dplyr, magrittr, tibble) # Load packages with pacm
 aqspec_url <- "https://www.aqmd.gov/aq-spec/evaluations/criteria-pollutants/summary-pm"
 aqspec_html <- read_html(aqspec_url)
 
+colnames <- c("Make", "Est. Cost ($)", "Pollutant", "Field R2", "Field MAE (µg/m3)", "Lab MAE (µg/m3)")
+
 pmsensor_table <- aqspec_html %>% 
   html_elements(".telerik-reTable-1") %>%
-  html_table() %>% .[[1]] %>% 
+  html_table() %>% .[[1]] %>%
   select(c(X2,X3,X4,X5,X8,X9)) %>% 
-  filter(!row_number() %in% 1)
+  filter(!row_number() %in% 1) # TODO Remove current columnheaders, replace with row 1 or colnames
+
 pmsensor_table
+colnames
 
 as.vector(pmsensor_table)
 pmsensor_table %>% filter()
