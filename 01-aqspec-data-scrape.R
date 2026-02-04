6b55f185ac662165ea57e9c7c602b6f31ee94af2
chris47 <chris47.27@proton.me>
2026-02-03 21:08:20 -0800

    Updated data scrape R script to work with new AQ-SPEC website

--- 01-aqspec-data-scrape.R
+++ 01-aqspec-data-scrape.R
@@ -1,36 +1,62 @@
 rm(list=ls()) # Start with a clean environment
 #install.packages("tidyverse")
-pacman::p_load(pacman, rvest, tidyr, dplyr, magrittr, stringr, visdat, gtExtras, ggplot2, viridis) # Load packages with pacman
+library(tidyverse)
+library(rvest)
+library(visdat)
+library(glue)
+#pacman::p_load(pacman, rvest, tidyr, dplyr, magrittr, stringr, visdat, gtExtras, ggplot2, viridis) # Load packages with pacman
 
 # Pull AQ-Spec website data
 aqspec_html <- read_html("https://www.aqmd.gov/aq-spec/evaluations/criteria-pollutants/summary-pm")
 
 # Create dataframe from website html and format data
 pmsensor_table <- aqspec_html %>% 
-  html_elements(".telerik-reTable-1") %>%
-  html_table() %>% .[[1]] %>%
-  select(make = X2,cost = X3,pollutant = X4,fieldr2 = X5,fieldmae = X8,labmae = X9) %>% 
-  filter(!row_number() %in% c(1,2) & pollutant == "PM2.5") %>%
-  select(!pollutant) %>%
+  # Select the HTML element name of the table.
+  # Find the element with https://rvest.tidyverse.org/articles/selectorgadget.html
+  html_elements(".backdrop") %>%
+  # Convert HTML table to dataframe
+  html_table() %>%  .[[1]] %>%
+  # Select desired columns and rename for clarity
+  select(model = "Make (Model)",
+         cost = "Est. Cost\n            (USD)",
+         pollutant = "Pollutant \n            Tested\n            (Sensor Type)",
+         fieldr2 = "Field R2",
+         fieldmae = "Field MAE\n            (PM: µg/m3\n            Gas: ppb)") %>% 
+  # Keep only rows from PM2.5 sensors
+  filter(pollutant == "PM2.5") %>% 
+  # Split fieldR2 and field MAE into separate low and high columns:
   separate_wider_delim(col = fieldr2, delim = " to ", names = c("fieldr2lo", "fieldr2hi"), too_few = "align_start") %>%
-  separate_wider_delim(col = fieldmae, delim = " to ", names = c("fieldmaelo", "fieldmaehi"), too_few = "align_start") %>%
-  separate_wider_delim(col = labmae, delim = " to ", names = c("labmaelo", "labmaehi"), too_few = "align_start") %>%
+  separate_wider_delim(col = fieldmae, delim = " to ", names = c("fieldmaelo", "fieldmaehi"), too_few = "align_start") %>% 
   mutate(
-    cost = as.numeric(gsub("[~\\$,]", "", cost)),
+    # Remove newline and extra spaces from model names
+    model = str_replace_all(model, "\\s*\\n\\s*", " "),
+    cost = as.numeric(cost),
+    pollutant = as.factor(pollutant),
     fieldr2lo = as.numeric(fieldr2lo),
     fieldr2hi = as.numeric(fieldr2hi),
-    fieldmaelo = as.numeric(na_if(fieldmaelo, "")),
-    fieldmaehi = as.numeric(fieldmaehi),
-    labmaelo = as.numeric(na_if(labmaelo, "")),
-    labmaehi = as.numeric(labmaehi),
+    # Set the "-" field MAE to NA
+    fieldmaelo = as.numeric(na_if(fieldmaelo, "-")),
+    fieldmaehi = as.numeric(fieldmaehi)
     )
 
-pmsensor_table
+# Write pmsensor_table to .csv with current date
+filename <- glue("./data/pmsensors_{Sys.Date()}.csv")
+write_csv(pmsensor_table, filename)
 
-# Visualise data
-vis_dat(pmsensor_table) # Visualize datatypes
-vis_miss(pmsensor_table) # Visualize missing values
-  
+## Explore data
+#glimpse(pmsensor_table)
+#View(pmsensor_table)
+#names(pmsensor_table)
+#pmsensor_table
+#
+## Visualise data with "visdat" package
+#vis_dat(pmsensor_table) # Visualize datatypes
+#vis_miss(pmsensor_table) # Visualize missing values
+
+#------------------------------------------------------------------------
+# 2026-02-03 Git all code working with new AQ-SPEC table up to this point
+# [ ] Move table creation and data visualization to another script
+
 # Create a table to view the data
 pmsensor_table %>%
   filter(fieldr2lo >= 0.7) %>%
